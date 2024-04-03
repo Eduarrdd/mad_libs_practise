2f92e6d8c7823896f0332fb561e05522e91f9ca0
Eduardo <eduar2011.er36@gmail.com>
2024-04-03 12:19:51 -0600

    Practising f-strings in python

---
+++ mad_libs.py
@@ -0,0 +1,10 @@
+
+# Madlib Project
+
+adjective = input('Adjective: ').lower()
+verb1 = input('Verb: ').lower()
+verb2 = input('Verb: ').lower()
+pluralnoun = input('Plural noun: ').lower()
+
+madlib = f"Program is so {adjective}! I always feel excited because I love to {verb1} issues. Learn how to {verb2} it and reach your {pluralnoun}"
+print(madlib)
